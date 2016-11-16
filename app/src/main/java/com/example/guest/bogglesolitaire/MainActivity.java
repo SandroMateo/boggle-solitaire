88b585c69f1f2ee6b7ee64e7786a35cf53ade3b1
Blake Scala and Sandro Alvarez <blake+sandro@>
2016-11-16 09:38:34 -0800

    add custom font to main title

--- app/src/main/java/com/example/guest/bogglesolitaire/MainActivity.java
+++ app/src/main/java/com/example/guest/bogglesolitaire/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.guest.bogglesolitaire;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.mainTitle) TextView mMainTitle;
+    @Bind(R.id.newGame) Button mNewGameButton;
+    @Bind(R.id.instructions) TextView mInstructions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        Typeface pacificoFont = Typeface.createFromAsset(getAssets(), "fonts/Pacifico.ttf");
+        mMainTitle.setTypeface(pacificoFont);
     }
 }
