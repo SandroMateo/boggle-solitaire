00db2a04586011fef6b938a490fa3062fcefa7b3
Blake Scala and Sandro Alvarez <blake+sandro@>
2016-11-16 09:56:55 -0800

    add game activity as child activity of main and add click listener to new game button to navigate there

--- app/src/main/java/com/example/guest/bogglesolitaire/MainActivity.java
+++ app/src/main/java/com/example/guest/bogglesolitaire/MainActivity.java
@@ -1,15 +1,17 @@
 package com.example.guest.bogglesolitaire;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     @Bind(R.id.mainTitle) TextView mMainTitle;
     @Bind(R.id.newGame) Button mNewGameButton;
     @Bind(R.id.instructions) TextView mInstructions;
@@ -22,5 +24,17 @@ public class MainActivity extends AppCompatActivity {
 
         Typeface pacificoFont = Typeface.createFromAsset(getAssets(), "fonts/Pacifico.ttf");
         mMainTitle.setTypeface(pacificoFont);
+
+        mNewGameButton.setOnClickListener(this);
+
+        mInstructions.setText(getString(R.string.instructions));
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == mNewGameButton) {
+            Intent intent = new Intent(MainActivity.this, GameActivity.class);
+            startActivity(intent);
+        }
     }
 }
